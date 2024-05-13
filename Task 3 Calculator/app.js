aee0bf6268fcf9dbdc934b5aa34665adc445cf18
Balla Kavya <ballakavya594@gmail.com>
2024-05-13 06:50:27 +0530

    Update | calculator

--- Task 3 Calculator/app.js
+++ Task 3 Calculator/app.js
@@ -1,13 +1,20 @@
 function appendToDisplay(value) {
-    document.getElementById("display").value += value;
+  document.getElementById("display").value += value;
+}
+
+function calculate() {
+  let expression = document.getElementById("display").value;
+  let result;
+
+  try {
+    result = eval(expression);
+  } catch (error) {
+    result = "Error";
   }
 
-  function calculate() {
-    let expression = document.getElementById("display").value;
-    let result = eval(expression);
-    document.getElementById("display").value = result;
-  }
+  document.getElementById("display").value = result;
+}
 
-  function clearDisplay() {
-    document.getElementById("display").value = "";
-  }
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
